1e4190cd0a64fe3e1905c5ffc5e2e255f38fafde
sunyuzhe <sunnior@gmail.com>
2018-05-13 14:22:27 +0800

    add file

--- cmake/tf_core_tests.cmake
+++ cmake/tf_core_tests.cmake
@@ -10,11 +10,14 @@ target_link_libraries(test_main ${gtest_static_library})
 file(GLOB_RECURSE tf_core_lib_test_srcs
     "${tensorflow_source_dir}/tensorflow/core/lib/*test*.h"
     "${tensorflow_source_dir}/tensorflow/core/lib/*test*.cc"
+    "${tensorflow_source_dir}/tensorflow/core/platform/*test*.h"
+    "${tensorflow_source_dir}/tensorflow/core/platform/*test*.cc"
 )
 
 add_executable(test_lib ${tf_core_lib_test_srcs})
-
 target_link_libraries(test_lib test_main tf_core_lib)
+
+
 
 #add_custom_command(
 #    TARGET test_lib_core 
