c294571da1aa27f6a5ba3178aa863d8034cb2daa
sunyuzhe <sunnior@gmail.com>
2018-05-29 08:22:13 +0800

    can loader conv

--- code/demo/loader/main.cc
+++ code/demo/loader/main.cc
@@ -8,15 +8,25 @@ int main() {
   using namespace tensorflow;
   using namespace tensorflow::ops;
   
-  const string export_dir = "code/demo/loader/test_dir";
+  const string export_dir = "code/demo/loader/test_model_conv";
   SavedModelBundle bundle;
   SessionOptions session_options;
   RunOptions run_options;
 
-  LoadSavedModel(session_options, run_options, export_dir, {"test_graph"}, &bundle);
+  LoadSavedModel(session_options, run_options, export_dir, {"test_model_conv"}, &bundle);
 
-  std::vector<std::pair<string, Tensor>> feeds{{"input_x", test::AsTensor<float>({1.0f})}};
-  std::vector<string> output_tensor_names{"target_add"};
+  float data[32][32][3];
+  float* pdata = &data[0][0][0];
+
+  for (int i = 0; i < 32*32*3; ++i)
+  {
+    *(pdata + i) = 1.0f;
+  }
+  gtl::ArraySlice<float> dataArray(pdata, 32*32*3);
+  Tensor input_tensor = test::AsTensor(dataArray, {1, 32, 32, 3});
+
+  std::vector<std::pair<string, Tensor>> feeds{{"input", input_tensor}};
+  std::vector<string> output_tensor_names{"relu"};
   std::vector<string> target_node_names;
   std::vector<Tensor> outputs;
 
@@ -24,5 +34,11 @@ int main() {
   bundle.session->Run(run_options, feeds, output_tensor_names, target_node_names, &outputs, nullptr);
   Tensor tensor = outputs[0];
   auto p_data = tensor.flat<float>();
+  int size = p_data.size();
+  float* p_out_data = p_data.data();
+  for (int i = 0; i < size; ++i)
+  {
+    printf("%f ", *(p_out_data + i));
+  }
   return 0;
 }
---
+++ code/demo/loader/model_conv.py
@@ -0,0 +1,33 @@
+import numpy as np
+import tensorflow as tf
+
+size = 32
+channel = 3
+
+kernel_size = 5
+conv_out_channel = 12
+
+v_input = np.full([1, size, size, channel], 1.0, dtype=np.float32)
+
+input = tf.placeholder(dtype=tf.float32, shape=[None, size, size, channel], name='input')
+filter = tf.Variable(tf.constant(1.0, dtype=tf.float32, shape=[kernel_size, kernel_size, channel, conv_out_channel]), name='filter')
+
+conv = tf.nn.conv2d(input, filter, strides=[1, 2, 2, 1], padding='SAME', name='conv')
+
+bias = tf.Variable(tf.constant(1.0, dtype=tf.float32, shape=[size/2, size/2, conv_out_channel]), name='bias')
+
+relu = tf.nn.relu(conv + bias, name='relu')
+
+#builder = tf.saved_model.builder.SavedModelBuilder('test_model_conv')
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    vout = sess.run(relu, feed_dict={input:v_input})
+    print(vout)
+    #builder.add_meta_graph_and_variables(sess, ['test_model_conv'])
+
+#builder.save()
+
+
+#cost = tf.reduce_sum(tf.pow(Y_pred - Y, 2)) / (n_observations - 1)
+
